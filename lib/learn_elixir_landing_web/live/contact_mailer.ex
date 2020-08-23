91629f8624610a1b00a67c2b55cc9e29452211cf
MikaAK <mika@kalathil.me>
2020-08-23 14:02:03 -0700

    feat: polish contact component and live view alerts

--- lib/learn_elixir_landing_web/live/contact_mailer.ex
+++ lib/learn_elixir_landing_web/live/contact_mailer.ex
@@ -5,9 +5,11 @@ defmodule LearnElixirLandingWeb.ContactMailerLive do
   alias LearnElixirLandingWeb.FormValidators
   alias LearnElixirLanding.Mailer
 
+  @flash_clear_delay :timer.seconds(5)
+
   def render(assigns) do
     ~L"""
-      <%= f = form_for @contact_changeset, "#", [phx_change: :validate, phx_submit: :save, id: "contact-form", class: ["form--stacked w5 center"]] %>
+      <%= f = form_for @contact_changeset, "#", [phx_change: :validate, phx_submit: :save, id: "contact-form", class: "form--stacked w-100 center"] %>
         <div class='form-group'>
           <%= label f, :email %>
           <%= email_input f, :email %>
@@ -22,12 +24,12 @@ defmodule LearnElixirLandingWeb.ContactMailerLive do
 
         <div class='form-group'>
           <%= label f, :message %>
-          <%= textarea f, :message %>
+          <%= textarea f, :message, class: 'h4' %>
           <%= error_tag f, :message %>
         </div>
 
         <div class='form-button'>
-          <%= submit "Save", disabled: !@contact_changeset.valid?, class: "button" %>
+          <%= submit "Send", disabled: !@contact_changeset.valid?, class: "button-small button-purple w-100" %>
         </div>
       </form>
     """
@@ -35,6 +37,10 @@ defmodule LearnElixirLandingWeb.ContactMailerLive do
 
   def mount(_params, %{}, socket) do
     {:ok, reset_form(socket)}
+  end
+
+  def handle_info(:clear_flash, socket) do
+    {:noreply, clear_flash(socket)}
   end
 
   def handle_event("validate", %{"contact_mailer" => params}, socket) do
@@ -48,15 +54,23 @@ defmodule LearnElixirLandingWeb.ContactMailerLive do
 
     case Mailer.send_contact_email(email, name, message) do
       {:ok, _} ->
-        socket = socket |> put_flash(:info, "Email sent successfully") |> reset_form
+        socket = socket |> put_flash(:success, "Email sent successfully") |> reset_form
+
+        setup_delayed_clear_flash()
 
         {:noreply, socket}
 
       {:error, e} ->
         Logger.error("Error sending emails: #{inspect e}")
 
+        setup_delayed_clear_flash()
+
         {:noreply, put_flash(socket, :error, "Email was unable to send, please try again later")}
     end
+  end
+
+  defp setup_delayed_clear_flash do
+    Process.send_after(self(), :clear_flash, @flash_clear_delay)
   end
 
   defp reset_form(socket) do
